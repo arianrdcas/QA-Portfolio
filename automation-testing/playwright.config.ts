e8f26c7bd33a7fd0b23dbf9ef14ac0f672d40217
arianrdcas <arianrdcas@gmail.com>
2026-03-19 23:19:30 -0300

    Anadiendo CI/CD

--- automation-testing/playwright.config.ts
+++ automation-testing/playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: process.env.CI ? true : false,
     //screenshot: "only-on-failure", // Toma captura automáticamente si el test falla
     //video: "retain-on-failure",
     /* Base URL to use in actions like `await page.goto('')`. */
